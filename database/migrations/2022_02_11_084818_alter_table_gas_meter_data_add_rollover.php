c31145dd17b7f22c1eca5cf92dee6012338bae44
theo <theo@jlab.org>
2022-02-11 15:46:45 -0500

    add rollover column to gas_meter_data for consistency

---
+++ database/migrations/2022_02_11_084818_alter_table_gas_meter_data_add_rollover.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableGasMeterDataAddRollover extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gas_meter_data', function($table){
+            $table->integer('rollover_accumulated')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gas_meter_data', function($table){
+            $table->dropColumn('rollover_accumulated');
+        });
+    }
+}
