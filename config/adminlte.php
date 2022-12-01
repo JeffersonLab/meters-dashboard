81408494e9dfe1c6523fa8e3b9445c2b36ed003b
Theo Larrieu <theo@jlab.org>
2022-12-01 13:49:33 -0500

    temporarily remove reports

--- config/adminlte.php
+++ config/adminlte.php
@@ -261,10 +261,10 @@ return [
 //        ],
 
         // Sidebar items:
-//        [
-//            'type' => 'sidebar-menu-search',
-//            'text' => 'search',
-//        ],
+        [
+            'type' => 'sidebar-menu-search',
+            'text' => 'search',
+        ],
         [   'text' => 'Reports',
             'url' => '/reports',
             'icon' => 'area-chart',
