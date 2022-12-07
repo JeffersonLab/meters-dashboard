e164881192f848799cd9a7fb068a2d6807c62da0
theo <theo@jlab.org>
2022-12-07 13:11:37 -0500

    restore from prior commit that deleted it and then clean up the switch statement.

---
+++ app/Reports/ReportFactory.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theo
+ * Date: 8/12/17
+ * Time: 11:01 AM
+ */
+
+namespace App\Reports;
+
+
+use Illuminate\Http\Request;
+
+class ReportFactory
+{
+    /**
+
+     */
+    static function make($name, Request $request){
+
+        switch ($name){
+            case 'power-consumption' : $report = new PowerConsumption(); break;
+            case 'water-consumption' : $report = new WaterConsumption(); break;
+            case 'gas-consumption' : $report = new GasConsumption(); break;
+            case 'multi-meter' : $report = new MultiMeter(); break;
+            case 'mya-stats' : $report = new MyaStats(); break;
+            default: $report = null;
+        }
+        if (! $report){
+            app()->abort('404', "The requested report is not available");
+        }
+        $report->applyRequest($request);
+        return $report;
+    }
+}
