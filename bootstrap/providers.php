271712d9545ea3e20748f4213a087cbbd59b1b92
Shift <shift@laravelshift.com>
2025-01-06 17:39:03 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\MacroServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\MenuServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -7,26 +7,6 @@ return [
 
     'mix_url' => env('MIX_ASSET_URL', null),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\MacroServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\MenuServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // ...
