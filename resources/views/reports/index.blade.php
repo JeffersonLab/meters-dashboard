0bd8e2f8e0a90d5e60814b00fe2670f5d55e11ef
theo <theo@jlab.org>
2025-11-26 08:34:32 -0500

    add export data to reports menu

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -22,6 +22,7 @@
                         <li>{!! Html::meterIcon('cooling-tower') !!}
                             {{ html()->a(route('reports.item', ['cooling-tower-consumption']), 'Cooling Tower Consumption') }}
                         </li>
+                        <li>{!! Html::meterIcon('database') !!} {{ html()->a(route('reports.item', ['export-data']), 'Spreadsheet Export') }}</li>
                     </ul>
 
 {{--                    <h4>{!! Html::meterIcon('power') !!} Power</h4>--}}
